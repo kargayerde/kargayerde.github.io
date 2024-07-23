036879a9a9e2fe40c7131ff2b44811842930dddf
kargayerde <51271164+kargayerde@users.noreply.github.com>
2024-07-23 03:03:46 +0300

    added new fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,68 @@
 function App() {
-	const pages = {
-		WASMbrot: "https://kargayerde.github.io/wasmbrot/",
-		balls: "https://kargayerde.github.io/balls/",
-		"More Discourses": "https://kargayerde.github.io/more-discourses/",
-		"Trippy Algebra": "https://kargayerde.github.io/trippy-algebra/",
-		Transposer: "https://kargayerde.github.io/transposer/",
-		Charfix: "https://kargayerde.github.io/charfix/",
-		"Parallax Scroller": "https://kargayerde.github.io/parallax-scroller/",
-		Chronos: "https://kargayerde.github.io/chronos/",
-		Gas: "https://kargayerde.github.io/gas/",
-		Pigraph: "https://kargayerde.github.io/pigraph/",
-	};
+	const pages = [
+		{
+			title: "WASMbrot",
+			link: "https://kargayerde.github.io/wasmbrot/",
+			repo: "",
+			description: "",
+		},
+		{ title: "balls", link: "https://kargayerde.github.io/balls/", repo: "", description: "" },
+		{
+			title: "chatnoir",
+			link: "https://chatnoir-front.onrender.com/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Trippy Algebra",
+			link: "https://kargayerde.github.io/trippy-algebra/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Pigraph",
+			link: "https://kargayerde.github.io/pigraph/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Parallax Scroller",
+			link: "https://kargayerde.github.io/parallax-scroller/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "More Discourses",
+			link: "https://kargayerde.github.io/more-discourses/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Chronos",
+			link: "https://kargayerde.github.io/chronos/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Transposer",
+			link: "https://kargayerde.github.io/transposer/",
+			repo: "",
+			description: "",
+		},
+		{
+			title: "Charfix",
+			link: "https://kargayerde.github.io/charfix/",
+			repo: "",
+			description: "",
+		},
+		{ title: "Gas", link: "https://kargayerde.github.io/gas/", repo: "", description: "" },
+	];
 
 	return (
 		<div className="App">
-			{Object.keys(pages).map((page, index) => (
+			{pages.map(({ title, link, repo, description }, index) => (
 				<div key={index}>
-					<a href={pages[page]}>{page.toLocaleLowerCase()}</a>
+					<a href={link}>{title.toLocaleLowerCase("tr")}</a>
 				</div>
 			))}
 		</div>
