0e7de7638dd34c34e37ed45a7247f2834b02f3f5
kargayerde <51271164+kargayerde@users.noreply.github.com>
2024-07-23 03:30:12 +0300

    added descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -4,65 +4,80 @@ function App() {
 			title: "WASMbrot",
 			link: "https://kargayerde.github.io/wasmbrot/",
 			repo: "",
-			description: "",
+			description: "A Mandelbrot viewer written in C and compiled to WASM.",
 		},
-		{ title: "balls", link: "https://kargayerde.github.io/balls/", repo: "", description: "" },
+		{
+			title: "balls",
+			link: "https://kargayerde.github.io/balls/",
+			repo: "",
+			description: "Dynamically generated 3D objects. Written in C, compiled to WASM",
+		},
 		{
 			title: "chatnoir",
 			link: "https://chatnoir-front.onrender.com/",
 			repo: "",
-			description: "",
+			description: "Full stack P2P encrypted group chat proof-of-work.",
 		},
 		{
 			title: "Trippy Algebra",
 			link: "https://kargayerde.github.io/trippy-algebra/",
 			repo: "",
-			description: "",
+			description: "2D patterns created using linear algebra and rendered to HTML canvas.",
 		},
 		{
 			title: "Pigraph",
 			link: "https://kargayerde.github.io/pigraph/",
 			repo: "",
-			description: "",
+			description: "Graph of digits of pi in different radixed rendered to HTML canvas.",
 		},
 		{
 			title: "Parallax Scroller",
 			link: "https://kargayerde.github.io/parallax-scroller/",
 			repo: "",
-			description: "",
+			description: "Parallax scrolling animation in HTML canvas.",
 		},
 		{
 			title: "More Discourses",
 			link: "https://kargayerde.github.io/more-discourses/",
 			repo: "",
-			description: "",
+			description:
+				"A tool to explore the relationships that emerge from Lacan's theory of four discourses.",
 		},
 		{
 			title: "Chronos",
 			link: "https://kargayerde.github.io/chronos/",
 			repo: "",
-			description: "",
+			description: "A timeline management tool.",
 		},
 		{
 			title: "Transposer",
 			link: "https://kargayerde.github.io/transposer/",
 			repo: "",
-			description: "",
+			description: "Transposes the chords in the input text without changing the rest.",
 		},
 		{
 			title: "Charfix",
 			link: "https://kargayerde.github.io/charfix/",
 			repo: "",
-			description: "",
+			description: "Fixes corrupted Turkish characters caused by encoding errors.",
 		},
-		{ title: "Gas", link: "https://kargayerde.github.io/gas/", repo: "", description: "" },
+		{
+			title: "Gas",
+			link: "https://kargayerde.github.io/gas/",
+			repo: "",
+			description:
+				"Gas price viewer and alarm widget for Ethereum using public APIs.",
+		},
 	];
 
 	return (
 		<div className="App">
 			{pages.map(({ title, link, repo, description }, index) => (
 				<div key={index}>
-					<a href={link}>{title.toLocaleLowerCase("tr")}</a>
+					<a href={link} target="#">
+						{title.toLocaleLowerCase("tr")}
+					</a>
+					<div>{description}</div>
 				</div>
 			))}
 		</div>
